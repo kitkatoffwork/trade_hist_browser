5e04b017f22f0408fe24134d8c5b7dc58ce477b9
Seiya Sasaki <sirukufarios@gmail.com>
2020-07-05 16:04:21 +0900

    removed 'material-ui' template,  added 'material-ui/core' template and tried to use it

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+// Design
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+  render() {
+    return (
+      <>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography>サンプルページ</Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+
+        <DemoTitle />
+      </>
+    );
+  }
+}
+
+function DemoTitle() {
   return (
     <div className="App">
       <header className="App-header">
