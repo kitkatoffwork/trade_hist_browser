aafd80621c49f84a6d305ba1bcfc84de5c081952
Seiya Sasaki <sirukufarios@gmail.com>
2020-08-02 15:58:13 +0900

    added the sample tests for reducers

---
+++ src/reducers/SampleRequesting.test.js
@@ -0,0 +1,27 @@
+import reducer from './SampleRequesting';
+
+describe('SampleRequesting Reducer', () => {
+  const initialState = { pareName: '', data: 'undefined', error: false }
+  test('初期値', () => {
+    const state = undefined;
+    const action = {};
+    const result = reducer(state, action);
+    const expected = initialState;
+
+    expect(result).toEqual(expected);
+  });
+
+  test('REQUEST アクション', () => {
+    const state = initialState;
+    const action = {
+      type: 'REQUEST',
+      payload: { pareName: 'USD_JPY' }
+    };
+    const result = reducer(state, action);
+    const expected = {
+      pareName: 'USD_JPY', data: 'undefined', error: false
+    };
+
+    expect(result).toEqual(expected);
+  });
+});
