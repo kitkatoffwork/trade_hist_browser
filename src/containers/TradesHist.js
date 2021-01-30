cda2a8ae6f0701f5ff3549f72eb37200676c4624
Seiya Sasaki <sirukufarios@gmail.com>
2021-01-30 09:25:26 +0900

    introduced redux-toolkit to tradeHist, and removed src/actions directory

--- src/containers/TradesHist.js
+++ src/containers/TradesHist.js
@@ -1,6 +1,47 @@
 import { connect } from 'react-redux';
 import TradesHist from '../components/TradesHist';
-import * as actions from '../actions/TradesHist';
+import {
+  // Actions
+  selectPair, setFromDatetime, setToDatetime,
+  request, receiveResponse, finishRequest
+} from '../reducers/TradesHist';
+
+const API_URL = process.env.REACT_APP_HIST_API_URL
+const requestHist = (pareName, fromISO, toISO) => {
+  return async dispatch => {
+    dispatch(request());
+
+    try {
+      const response = await fetch(
+        API_URL + `?pareName=${pareName}&from=${fromISO}&to=${toISO}`, {
+          // mode: 'cors' // なくてもCORS対応できてる
+          // headers: {'Content-Type': 'application/json'},
+          // method: 'POST',
+          // body: JSON.stringify({aa: 'dwa', bere: 13456})
+        }
+      );
+      const parsed_response = await response.json();
+      const status = await response.status;
+
+      if (status === 200) {
+        const history = parsed_response.history;
+        dispatch(receiveResponse(
+          { pareName: pareName, errorMsg: null, history: history }
+        ));
+      } else {
+        dispatch(receiveResponse(
+          { pareName: pareName, errorMsg: parsed_response.message }
+        ));
+      }
+    } catch (error) {
+      dispatch(receiveResponse(
+        { pareName: pareName, errorMsg: error.message })
+      );
+    } finally {
+      dispatch(finishRequest(pareName));
+    }
+  };
+};
 
 const mapStateToProps = (state, ownProps) => ({
   // pareName: ownProps.pareName,
@@ -17,19 +58,19 @@ const mapDispatchToProps = (dispatch) => ({
   //   dispatch(actions.requestHist(pareName));
   // },
   onUpdate(pareName) {
-    dispatch(actions.requestHist(pareName));
+    dispatch(requestHist(pareName));
   },
   selectPair(pairName) {
-    dispatch(actions.selectPair(pairName));
+    dispatch(selectPair(pairName));
   },
   setFromDatetime(fromISO) {
-    dispatch(actions.setFromDatetime(fromISO));
+    dispatch(setFromDatetime(fromISO));
   },
   setToDatetime(toISO) {
-    dispatch(actions.setToDatetime(toISO));
+    dispatch(setToDatetime(toISO));
   },
   request(pareName, fromISO, toISO) {
-    dispatch(actions.requestHist(pareName, fromISO, toISO));
+    dispatch(requestHist(pareName, fromISO, toISO));
   },
 });
 
